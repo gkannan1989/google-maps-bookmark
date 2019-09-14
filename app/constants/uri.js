5b96e5eb255688d48d73eb5da8a37e5a9e60e949
gkannan1989 <40201213+gkannan1989@users.noreply.github.com>
2019-09-15 00:32:37 +0800

    Update uri.js

--- app/constants/uri.js
+++ app/constants/uri.js
@@ -2,8 +2,8 @@ import SERVER_INFO from "./serverInfo";
 const URI = {
   GOOGLE_MAP: {
     TEXT_SEARCH: ({ query }) => 
-    `${SERVER_INFO.uri}/api/search/suggestion?query=${query}`
+    `${SERVER_INFO.uri}${process.env.SUGGESTION_PATH}?query=${query}`
   },
-  BOOKMARK_END_POINT: `${SERVER_INFO.uri}/api/v1/bookmarks`
+  BOOKMARK_END_POINT: `${SERVER_INFO.uri}${process.env.BOOKMARK_PATH}`
 };
 export default URI;
