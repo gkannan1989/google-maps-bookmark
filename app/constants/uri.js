76b0a820dd34fac299ef011d8065ff59fde4132c
gkannan1989 <40201213+gkannan1989@users.noreply.github.com>
2019-09-14 18:31:53 +0800

    Update uri.js

--- app/constants/uri.js
+++ app/constants/uri.js
@@ -4,5 +4,6 @@ const URI = {
     TEXT_SEARCH: ({ query }) => 
     `${SERVER_INFO.uri}/api/search/suggestion?query=${query}`
   },
+  BOOKMARK_END_POINT: `${SERVER_INFO.uri}/api/v1/bookmarks`
 };
 export default URI;
